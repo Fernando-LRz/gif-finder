9af26dd320b100b6571d73d3765f9389688e343b
Fernando LR <fernando.lorenzo1899@gmail.com>
2023-09-08 19:59:57 -0600

    add .env file and remove api key after deactivation

--- src/helpers/getGifs.js
+++ src/helpers/getGifs.js
@@ -1,7 +1,7 @@
 
 const getGifs = async (category) => {
-        
-    const apiKey = 'VY2KaXK2VLvqZJQgqKZfKpWTMneC7o47';
+
+    const apiKey = import.meta.env.VITE_API_KEY;
     const url = `https://api.giphy.com/v1/gifs/search?api_key=${apiKey}&q=${category}&limit=10`;
 
     const resp = await fetch(url);
